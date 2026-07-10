aee4746fd25912a2eef7d58941cdf063c379758a
dmtress444-lsg <dmtress444@gmail.com>
2026-07-10 18:03:08 +0000

    version1 del dashboard

--- hello-dash.py
+++ hello-dash.py
@@ -1,1 +1,6 @@
 import streamlit as st
+import pandas as pd
+st.title("hello world web")
+st.write("hellow world streamlit")
+dataframe= pd.read_csv ("https://raw.githubusercontent.com/dmtress444-lsg/datasets/refs/heads/main/titanic.csv")
+st.dataframe(dataframe) 
